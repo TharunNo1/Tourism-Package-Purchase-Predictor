33cee520b3b954dab404e4af1193857977df95e3
Tharun G <65439029+TharunNo1@users.noreply.github.com>
2026-07-26 18:30:51 +0530

    update tourism_project/deployment/app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -58,7 +58,7 @@ except Exception as e:
 # Sidebar
 with st.sidebar:
     st.image(
-        "http://unsplash.com/photos/man-taking-photo-of-hot-air-balloons-eOcyhe5-9sQ",
+        "https://images.unsplash.com/photo-1530789253388-582c481c54b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         caption="Wellness Tourism Intelligence",
         use_container_width=True,
     )
